8f2040247a3eb08ed49b03efb1e45b1a896bb011
Rushi Khot <rushikeshkhot2001@gmail.com>
2026-03-28 15:24:39 +0530

    Creating rag_llm_final_iitg_aiims.py with necessary class and function definitions.

---
+++ src/rag_llm_final_iitg_aiims.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+class ExperimentLogger:
+    # Implementation of ExperimentLogger
+    pass
+
+class CacheManager:
+    # Implementation of CacheManager
+    pass
+
+class OptimizedRAGIndexer:
+    # Implementation of OptimizedRAGIndexer
+    pass
+
+class OptimizedParameterTuner:
+    # Implementation of OptimizedParameterTuner
+    pass
+
+class ComprehensiveEvaluator:
+    # Implementation of ComprehensiveEvaluator
+    pass
+
+
+def clean_text(text):
+    # Implementation of clean_text
+    pass
+
+def read_pdf(pdf_path):
+    # Implementation of read_pdf
+    pass
+
+
+def load_documents(doc_paths):
+    # Implementation of load_documents
+    pass
+
+
+def split_documents(documents, chunk_size):
+    # Implementation of split_documents
+    pass
+
+
+def rag_query(query):
+    # Implementation of rag_query
+    pass
+
+
+def rag_generate(inputs):
+    # Implementation of rag_generate
+    pass
+
+
+def vanilla_query(inputs):
+    # Implementation of vanilla_query
+    pass
+
+
+# Evaluation Section
+
+# Here you would add your evaluation code,
+# including metrics calculation and result logging.
